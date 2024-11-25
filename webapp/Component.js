1b5c72e8eaf0a6228b02f9ca09d2cae8df0ebf08
bandsan01 <sandeep.bandam@bakerhughes.com>
2024-11-25 11:44:36 +0000

    First Commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.app.zuiprecorderlist.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
